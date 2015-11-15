b2209c8cc0eb20f87a4d6479cafc1618cc848e0f
mkc1 <coulism@gmail.com>
2015-11-15 23:52:23 +0000

    nitrous-backup at Sun Nov 15 23:52:22 UTC 2015

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{(array.index(name)+1)} in line."
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    serving = array[0]
+    puts "Currently serving #{serving}."
+    array.shift
+  end
+end
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+    array.each { |i| puts i }
+  end
+end
+
+
+
+katz_deli = []
